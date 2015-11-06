92382a295e90e3fd2904ff02221104acda8f0c02
alexey1234 <alexeykr68@gmail.com>
2015-11-06 19:20:14 +0200

    Version 2

--- ext/minidlna/extensions_minidlna_config.php
+++ ext/minidlna/extensions_minidlna_config.php
@@ -2,7 +2,7 @@
 /*
 extensions_minidlna_config.php
 */
-define (MINIDLNA_VERSION,1);
+define (MINIDLNA_VERSION,2);
 require("auth.inc");
 require("guiconfig.inc");
 require("ext/minidlna/function.php");
