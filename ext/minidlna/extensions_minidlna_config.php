1ad0872f65af882a8bdc6a5dc5808dfb7aec0229
alexey1234 <alexeykr68@gmail.com>
2015-11-11 23:08:19 +0200

    Update extensions_minidlna_config.php
    Update version

--- ext/minidlna/extensions_minidlna_config.php
+++ ext/minidlna/extensions_minidlna_config.php
@@ -2,7 +2,7 @@
 /*
 extensions_minidlna_config.php
 */
-define (MINIDLNA_VERSION,2);
+define (MINIDLNA_VERSION,3);
 require("auth.inc");
 require("guiconfig.inc");
 require("ext/minidlna/function.php");
